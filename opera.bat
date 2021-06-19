fe128af497413bc8aa6b36087b488eb8d7e3afd4
bikinlagix991 <85218804+bikinlagix991@users.noreply.github.com>
2021-06-19 10:57:09 +0700

    Update opera.bat

--- opera.bat
+++ opera.bat
@@ -1,1 +1,1 @@
-start /wait Opera_47.0.2631.71_Setup_x64.exe /SILENT /norestart /allusers=yes /launchopera=no /desktopshortcut=no /setdefaultbrowser=no /pintotaskbar=no
+start /wait Opera.exe /SILENT /norestart /allusers=yes /launchopera=no /desktopshortcut=no /setdefaultbrowser=no /pintotaskbar=no
